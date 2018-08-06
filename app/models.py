1c4e70d20558be5d3b88ee428697f740e6bc5663
Yuraist <yura.ist@icloud.com>
2018-08-06 11:31:54 +0300

    Created different user roles

--- app/models.py
+++ app/models.py
@@ -4,9 +4,61 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 from . import login_manager
 
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
+
+class Permission:
+    CREATE_MODEL = 1
+    SEE_MODEL = 2
+    CREATE_MANAGER = 4
+    ADMINISTER = 64
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    permissions = db.Column(db.Integer)
+    default = db.Column(db.Boolean, default=False, index=True)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    @staticmethod
+    def insert_roles():
+        roles = {
+            'Manager': [Permission.CREATE_MODEL],
+            'Administrator': [Permission.CREATE_MODEL, Permission.CREATE_MANAGER,
+                              Permission.SEE_MODEL, Permission.ADMINISTER]
+        }
+
+        default_role = 'Administrator'
+        for r in roles:
+            role = Role.query.filter_by(name=r).first()
+            if role is None:
+                role = Role()
+                role.name = r
+            role.reset_permission()
+            for perm in roles[r]:
+                role.add_permission(perm)
+            role.default = (role.name == default_role)
+            db.session.add(role)
+        db.session.commit()
+
+    def add_permission(self, perm):
+        if not self.has_permission(perm):
+            self.permissions += perm
+
+    def remove_permission(self, perm):
+        if self.has_permission(perm):
+            self.permissions -= perm
+
+    def reset_permission(self):
+        self.permissions = 0
+
+    def has_permission(self, perm):
+        return self.permissions & perm == perm
 
 
 class User(UserMixin, db.Model):
@@ -15,12 +67,11 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), unique=True, index=True)
     password_hash = db.Column(db.String(128))
 
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     avatar_id = db.Column(db.Integer, db.ForeignKey('photos.id'))
 
     models = db.relationship('Model', backref='manager')
     archive = db.relationship('Contract', backref='manager', lazy='dynamic')
-
-    # TODO: - Create different roles
 
     @property
     def password(self):
